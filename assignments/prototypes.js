1ec518e36d48108482a6d690e02cc8eb6d0683ef
Vijay Das <vijaytdas@gmail.com>
2019-04-03 17:31:43 -0500

    interim commit - both assignments still in progress

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,25 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject (attributes){
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function(){
+  return `${this.name} was removed from the game.`
+}
+
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+const CharacterStats = new GameObject
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -41,7 +54,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +115,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. global/window binding: this will refer to the "the forest"
+* 2. implicit binding: most common, this refers to what is left of the dot.
+* 3. new binding: comes from the constructor function with the keyword "new"
+* 4. explicit bonding: uses the array functions: .call, .apply, and .bind to explicitly state the referral
 *
 * write out a code example of each explanation above
 */
@@ -13,7 +13,16 @@
 
 // code example for Window Binding
 
+function windowThis(name){
+    console.log(this.name);
+    return name;
+}
+
+windowThis("Viva");
+
 // Principle 2
+
+
 
 // code example for Implicit Binding
 
